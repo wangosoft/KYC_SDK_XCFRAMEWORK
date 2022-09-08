6a6e1d90df987e80a0498a244f705e602ab505fc
Onur GÜLER <info@onrglr.com>
2022-09-08 21:08:50 +0300

    Add files via upload

---
+++ KYC_SDK.xcframework/ios-arm64/KYC_SDK.framework/Headers/KYC_SDK.h
@@ -0,0 +1,18 @@
+//
+//  KYC_SDK.h
+//  KYC_SDK
+//
+//  Created by Onur GÜLER on 18.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KYC_SDK.
+FOUNDATION_EXPORT double KYC_SDKVersionNumber;
+
+//! Project version string for KYC_SDK.
+FOUNDATION_EXPORT const unsigned char KYC_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYC_SDK/PublicHeader.h>
+
+
